20c586ef128485367728b672213c03e99c8c399b
ghtjfk <138800222+ghtjfk@users.noreply.github.com>
2025-09-26 17:27:53 +0900

    오류 수정
    오류 수정

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -6,9 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    //public Button startButton;
-
-    
 
     private void Awake()
     {
@@ -17,26 +14,7 @@ public class GameManager : Singleton<GameManager>
 
     void Start()
     {
-        // 버튼 클릭 시 OnStartButtonClick 실행
-        //startButton.onClick.AddListener(OnStartButtonClick);
-        LoadBrickMap();
-        LoadHP();
-    }
-    
-
-    //private void OnStartButtonClick()
-    //{
-    //    SceneManager.LoadScene("GameScene");
-    //}
-    private void LoadBrickMap()
-    {
-        SceneManager.LoadScene("BrickScene", LoadSceneMode.Additive);
-    }
-
-    void LoadHP()
-    {
-        SceneManager.LoadScene("UIScene", LoadSceneMode.Additive);
-        SceneManager.LoadScene("MapScene");
+        
     }
 
     //데이트 왕창~
