a1a412ababa6ca954393c0f2b64d357f6136210a
duckduck2438 <duckduck2438@naver.com>
2025-12-17 14:43:28 +0900

    조금 수정
    조금 수정

--- Assets/02.Scripts/HPUIManager.cs
+++ Assets/02.Scripts/HPUIManager.cs
@@ -28,7 +28,6 @@ public class HPUIManager : Singleton<HPUIManager>, IPointerEnterHandler, IPointe
     }
     string GetHPText()
     {
-        GameManager.Instance.LoadData();
         float curruntHP = GameManager.Instance.nowPlayer.curruntHP;
         float maxHP = GameManager.Instance.nowPlayer.maxHP;
         return $"{curruntHP} / {maxHP}";
