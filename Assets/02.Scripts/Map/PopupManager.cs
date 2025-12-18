4f331138b483277c575a99fa4314f2d6ab739b32
loirouge414 <jhpark60104@naver.com>
2025-12-18 17:10:44 +0900

    MapScene popup update

--- Assets/02.Scripts/Map/PopupManager.cs
+++ Assets/02.Scripts/Map/PopupManager.cs
@@ -50,7 +50,7 @@ public class PopupManager : MonoBehaviour
         pendingStage = stageNumber;
 
         if (messageText != null)
-            messageText.text = $"스테이지 {stageNumber}에 입장하시겠습니까?";
+            messageText.text = $"스테이지 {stageNumber}에\n\n입장하시겠습니까?";
 
         if (popupRoot != null && !popupRoot.activeSelf)
             popupRoot.SetActive(true);
